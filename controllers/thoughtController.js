0b4c79d5224a530b94930150c6d33b29c342b20b
Lilac <jonasjosh7@gmail.com>
2023-09-22 16:09:45 -0600

    fixed get routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,6 +6,7 @@ module.exports = {
     async getThoughts(req, res) {
         try {
             const thoughts = await Thought.find();
+            res.json(thoughts);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,9 @@ module.exports = {
     async getUsers(req, res) {
         try {
             const users = await User.find();
-        } catch (err) {
+            res.json(users);
+        } 
+        catch (err) {
             console.log(err);
             return res.status(500).json(err);
         }
